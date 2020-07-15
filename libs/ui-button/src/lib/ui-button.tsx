1ab413ab60d111136eaef502c908eda20b5b3cf9
Isaac Mann <isaacplmann+git@gmail.com>
2020-07-15 12:51:19 -0400

    step11 generate a library

---
+++ libs/ui-button/src/lib/ui-button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './ui-button.css';
+
+/* eslint-disable-next-line */
+export interface UiButtonProps {}
+
+export const UiButton = (props: UiButtonProps) => {
+  return (
+    <div>
+      <h1>Welcome to ui-button!</h1>
+    </div>
+  );
+};
+
+export default UiButton;
